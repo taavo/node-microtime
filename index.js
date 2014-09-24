12e4278a66cdf0b3f61a111fc984f5c48e8e33d5
Grant Hutchins and Taavo Smith <pair+ghutchins+tsmith@pivotal.io>
2014-09-24 14:01:50 -0400

    Use node-pre-gyp to build binary

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var binding = require('bindings')('microtime.node')
+var binary = require('node-pre-gyp');
+var path = require('path');
+var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
+var binding = require(binding_path);
 
-exports.now = binding.now
-exports.nowDouble = binding.nowDouble
-exports.nowStruct = binding.nowStruct
+exports.now = binding.now;
+exports.nowDouble = binding.nowDouble;
+exports.nowStruct = binding.nowStruct;
